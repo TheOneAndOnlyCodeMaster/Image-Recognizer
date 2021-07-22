3a7aff30173886447e16068bd01fa702e396e598
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2021-07-22 15:52:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -14,5 +14,19 @@ function takePicture(){
 console.log('ml5 version', ml5.version)
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Nl9dLzAab/model.json', modelLoaded);
 function modelLoaded(){
-
+console.log("model loaded");
+}
+function recognize(){
+    picture = document.getElementById("taken_photo");
+    classifier.classify(picture, gotResults);
+    }
+function gotResults(error, result){
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log(result);
+            document.getElementById("name").innerHTML=result[0].label;
+            document.getElementById("accuracy").innerHTML=result[0].confidence.toFixed(3)*100+"%";
+        }
 }
